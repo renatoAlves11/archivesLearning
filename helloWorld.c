7bfa1c6348d427d564141c77f369a65d5515c918
renatoAlves11 <132839322+renatoAlves11@users.noreply.github.com>
2024-10-03 20:38:00 -0300

    Add files via upload

---
+++ helloWorld.c
@@ -0,0 +1,23 @@
+//transiente - temporaria, não persistente
+//arquivo: sequência de informação persistente
+//disco: como grande array que começa no 0 até espaço disponível(ex: 1TB)
+//bloco: unidade de transferência de arquivos
+//"apagar" um arquivo apenas excluí a sua referência pelo sistema, mas as informações se mantém no disco
+//para realmente apagar, ou se sobrescreve os dados ou se destrói o disco
+
+//argc: quantos parametros
+//argv: lista de strings
+//argv[0] é o nome do programa
+
+//java não passa nome do programa como primeiro argumento
+
+//return 1 na main: deu ruim
+//return 0: td certo
+//precisa estar na documentação do seu programa
+
+//EOF: fim do arquivo -> só existe após uma ~tentativa~ de leitura frustrada
+
+//retorno de fread é a quantidade de elementos que você conseguiu ler
+
+
+                                                      
