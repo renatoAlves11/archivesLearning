df21cb423581ec7d3bfd6397030965c178cd99b2
renatoAlves11 <132839322+renatoAlves11@users.noreply.github.com>
2024-10-03 20:37:27 -0300

    Delete helloWorld.c

--- helloWorld.c
+++
@@ -1,8 +0,0 @@
-//transiente - temporaria, não persistente
-//arquivo: sequência de informação persistente
-//disco: como grande array que começa no 0 até espaço disponível(ex: 1TB)
-//bloco: unidade de transferência de arquivos
-//"apagar" um arquivo apenas excluí a sua referência pelo sistema, mas as informações se mantém no disco
-//para realmente apagar, ou se sobrescreve os dados ou se destrói o disco
-
-                                                      
