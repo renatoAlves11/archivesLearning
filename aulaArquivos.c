1fed512d6eb55338992ba3e213b8055b04057820
Renato Alves de Sousa <renato.alvs1103@gmail.com>
2024-12-12 21:15:00 -0300

    Add files via upload

---
+++ aulaArquivos.c
@@ -0,0 +1,18 @@
+//map = dicionario -> chave/valor
+//árvore é O(logn)
+//HashMap é O(1)
+//encontrar cpf(chave) baseado na operação do resto da divisão entre os 2 últimos dígitos do cpf por 11(seu tamanho).
+
+
+//colisão -> sobrescrita no hashm map
+
+//solução -> encadeamento externo: lista encadeada no índice do map, três tabelas (chave, valor, ponteiro)
+//externo pois está fora da tabela original
+
+//outra opção é escolher outro espaço da estrutura para armazenar a chave/valor o que pode atrapalhar na busca
+
+//ponteiro sera o numero utilizado para encontrar informações com fseek
+
+//livro silbersschatz / navatchi -> banco de dados
+
+//cosseno e seno muito usados para gerar imprevisibilidade no codigo
